6b8e6dcf40cefb9e44ecb117642e3b7fabd5e442
alphadzingina <alphaadx6@gmail.com>
2022-02-18 04:23:23 +0100

    move snake body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 #create a new screen
 screen = Screen()
@@ -12,6 +13,7 @@ starting_postions = [(0, 0), (-20, 0), (-40, 0)]
 
 segments = []
 
+#put snake body on screen
 for position in starting_postions:
     new_segment = Turtle("square")
     new_segment.color("white")
@@ -23,10 +25,16 @@ for position in starting_postions:
 game_is_on = True
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) -1, 0, -1): #(start=2, end=0, step=-1)
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
 
 
+    segments[0].forward(20)
 
 
 
