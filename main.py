956f67f1f890277cbad2bd7f47ebcbef423e187c
alphadzingina <alphaadx6@gmail.com>
2022-02-18 01:42:08 +0100

    setup screen

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import Screen, Turtle
+
+#create a new screen
+screen = Screen()
+
+#setup screen
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
