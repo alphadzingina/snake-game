bdb2c44dc652c2fb81cc9aabfd3f4c9557181d14
alphadzingina <alphaadx6@gmail.com>
2022-02-18 01:55:29 +0100

    move snake body

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ screen.title("Snake Game")
 
 starting_postions = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_postions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
+game_is_on = True
+
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
